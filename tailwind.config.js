4e3af3b50db16fb90c547576557610527e4beb66
danihermes <danielhn@gmail.com>
2024-05-02 15:20:49 -0400

    tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["/src/**/*.js ", "./index.html"],
+  content: ["/src/DrumPad.js ", "./index.html"],
   theme: {
     extend: {},
   },
